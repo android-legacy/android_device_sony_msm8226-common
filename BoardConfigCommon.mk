e86eab8efa8e10d50dc78e6938447f39b9530c43
erikcas <erikcas1972@gmail.com>
2014-11-30 16:02:58 +0100

    allow old ion flags
    Change-Id: Ic12d6770d88709e2e20fc622f80b42e2cfc4fafb

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -33,6 +33,7 @@ TARGET_CPU_VARIANT := krait
 TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
+COMMON_GLOBAL_CFLAGS += -D_ION_HEAP_MASK_COMPATIBILITY_WA
 
 # Kernel information
 BOARD_KERNEL_BASE     := 0x00000000
