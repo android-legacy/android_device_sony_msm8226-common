b89656dc0d80a7624cf07d5279546d14589a12dd
erikcas <erikcas1972@gmail.com>
2014-12-30 21:32:57 +0100

    drop gps
    Change-Id: I509b5e2e427983fc29da6ffca718076355157c87

--- msm8226-common.mk
+++ msm8226-common.mk
@@ -106,13 +106,6 @@ PRODUCT_PACKAGES += \
     FM2 \
     FMRecord
 
-# GPS
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/gps/flp.conf:system/etc/flp.conf \
-    $(COMMON_PATH)/gps/gps.conf:system/etc/gps.conf \
-    $(COMMON_PATH)/gps/izat.conf:system/etc/izat.conf \
-    $(COMMON_PATH)/gps/sap.conf:system/etc/sap.conf
-
 PRODUCT_PACKAGES += \
     com.qualcomm.location \
     gps.msm8226
